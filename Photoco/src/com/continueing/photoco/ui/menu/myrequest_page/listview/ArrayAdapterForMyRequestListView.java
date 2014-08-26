fafc73d53b7dc59bf3089ec337879612a3142640
HwangJaeYoung <forest62590@naver.com>
2014-08-26 16:48:59 +0900

    auto login

--- Photoco/src/com/continueing/photoco/ui/menu/myrequest_page/listview/ArrayAdapterForMyRequestListView.java
+++ Photoco/src/com/continueing/photoco/ui/menu/myrequest_page/listview/ArrayAdapterForMyRequestListView.java
@@ -2,11 +2,11 @@ package com.continueing.photoco.ui.menu.myrequest_page.listview;
 
 import android.content.Context;
 
-import com.continueing.photoco.reuse.listview.mvc.AbstractArrayImageViewAdapter;
+import com.continueing.photoco.reuse.listview.mvc.AbstractArrayAdapter;
 import com.continueing.photoco.reuse.listview.mvc.AbstractViewForListViewItem;
 import com.continueing.photoco.ui.menu.myrequest_page.listview.ViewForMyRequestListViewItem.IMyRequestItem;
 
-public class ArrayAdapterForMyRequestListView extends AbstractArrayImageViewAdapter<IMyRequestItem> {
+public class ArrayAdapterForMyRequestListView extends AbstractArrayAdapter<IMyRequestItem> {
 	
 	public ArrayAdapterForMyRequestListView(Context context, int resource) {
 		super(context, resource);
